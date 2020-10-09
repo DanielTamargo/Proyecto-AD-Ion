9257567a4817b7e2c362552193d66cf3789b524a
Irune <irune.mendez@ikasle.egibide.org>
2020-10-09 12:12:50 +0200

    parteIrune - modelos (clases con atributos)

---
+++ Irune/ParteIrune/src/com/company/Modelo/Cliente.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Cliente {
+
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private String profesion;
+    private String contrasenya;
+
+    public Cliente() {
+    }
+
+    public Cliente(String dni, String nombre, String apellidos, int edad, String profesion, String contrasenya) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.profesion = profesion;
+        this.contrasenya = contrasenya;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+}
---
+++ Irune/ParteIrune/src/com/company/Modelo/Empleado.java
@@ -0,0 +1,92 @@
+package com.company;
+
+public class Empleado {
+
+    private String dni;
+    private String nombre;
+    private String primerapellido;
+    private Date fechaNac;
+    private Date fechaContratacion;
+    private String nacionalidad;
+    private String cargo;
+    private String contrasenya;
+
+    public Empleado() {
+    }
+
+    public Empleado(String dni, String nombre, String primerapellido, Date fechaNac, Date fechaContratacion,
+                    String nacionalidad, String cargo, String contrasenya) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.primerapellido = primerapellido;
+        this.fechaNac = fechaNac;
+        this.fechaContratacion = fechaContratacion;
+        this.nacionalidad = nacionalidad;
+        this.cargo = cargo;
+        this.contrasenya = contrasenya;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerapellido() {
+        return primerapellido;
+    }
+
+    public void setPrimerapellido(String primerapellido) {
+        this.primerapellido = primerapellido;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(Date fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+}
