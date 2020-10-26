021e0cdf3390f528820bfe1d3aedb93f78544859
Daniel Tamargo <danieltamargosaiz@gmail.com>
2020-10-26 16:36:43 +0100

    EmpezandoMetadatos

---
+++ Dani/ParteDani/src/com/company/Metadatos.java
@@ -0,0 +1,102 @@
+package com.company;
+
+import com.company.sql.Conexiones;
+
+import java.sql.*;
+
+public class Metadatos {
+
+    public String nombresTablas = "empleados clientes visitas visitasclientes bonificaciones bonificacionesclientes registrosempleados registrosclientes" ;
+
+    public Metadatos() { }
+
+    public static void main(String[] args) {
+        new Metadatos().generarMetadatos(1);
+    }
+
+    /**
+     * Método reutilizado por los distintos métodos para cargar los datos que necesitemos
+     *
+     * @param bbdd <- dependiendo de este parámetro devolveremos una conexión u otra
+     * @return <- devuelve la variable conexión
+     */
+    public Connection realizarConexion(int bbdd) {
+        Connection conexion;
+        Conexiones conexiones = new Conexiones();
+        switch (bbdd) {
+            case 1 -> conexion = conexiones.conexionSQlite();
+            case 2 -> conexion = conexiones.conexionMySQL();
+            case 3 -> conexion = conexiones.conexionOracle();
+            default -> conexion = null;
+        }
+        return conexion;
+    }
+
+    /**
+     * Método reutilizado por las distintas BBDD para generar los metadatos de cada una de ellas
+     *
+     * @param bbdd <- dependiendo de este parámetro utilizaremos una conexión u otra
+     */
+    public void generarMetadatos(int bbdd) {
+        Connection conexion = realizarConexion(bbdd);
+
+        if (conexion != null) {
+            try {
+                DatabaseMetaData dbmd = conexion.getMetaData(); //Creamos objeto DatabaseMetaData
+                String nombre = dbmd.getDatabaseProductName();
+                String driver = dbmd.getDriverName();
+                String versionDriver = dbmd.getDriverVersion();
+                String url = dbmd.getURL();
+                String usuario = dbmd.getUserName();
+
+                System.out.println("INFORMACION SOBRE LA BASE DE DATOS: " + nombre);
+                System.out.println("Driver            : " + driver);
+                System.out.println("Versión del Driver: " + versionDriver);
+                System.out.println("URL               : " + url);
+                System.out.println("Usuario           : " + usuario);
+                System.out.println("=============================================");
+                System.out.println();
+                ResultSet resul;
+                ResultSet rsColumnas;
+                resul = dbmd.getTables("grupo3", null, null, null);
+
+                while (resul.next()) {
+                    String catalogo = resul.getString(1);//columna 1 que devuelve ResulSet
+                    String esquema = resul.getString(2); //columna 2
+                    String tabla = resul.getString(3);   //nombre de la tabla
+                    String tipo = resul.getString(4);  //tipo
+                    if (nombresTablas.contains(tabla.toLowerCase())) {
+                        System.out.println(tipo + " - Catalogo: " + catalogo + ", Esquema : " + esquema + ", Tabla : " + tabla);
+                        System.out.println();
+
+                        try {
+                            //Devuelve las PK de cada tabla de la BBDD
+                            ResultSet rsp = dbmd.getPrimaryKeys(null, null, tabla);
+                            if (rsp.next())
+                                System.out.println("PK (clave primaria): " + rsp.getObject(4));
+                            rsp.close();
+                        } catch (SQLException ignored) {
+                        }
+                        //Mostramos el nombre de las columnas y su tipo de dato
+                        rsColumnas = dbmd.getColumns(null, null, tabla, null);
+                        System.out.println("Columnas " + tabla);
+                        while (rsColumnas.next()) {
+                            System.out.format("\t %-17s | %s\n",
+                                     rsColumnas.getString(4), rsColumnas.getString("TYPE_NAME"));
+                        }
+                        System.out.println("=============================================");
+                        rsColumnas.close();
+                    }
+                }
+                resul.close();
+                conexion.close();
+
+            } catch (SQLException cn) {
+                cn.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
