55c2fd1b56495f44aa25c17ab6afbd24c4fcb01b
Daniel Tamargo <danieltamargosaiz@gmail.com>
2020-10-26 19:07:39 +0100

    metadatos

--- Dani/ParteDani/src/com/company/Metadatos.java
+++ Dani/ParteDani/src/com/company/Metadatos.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.sql.Conexiones;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Metadatos {
 
@@ -11,7 +12,7 @@ public class Metadatos {
     public Metadatos() { }
 
     public static void main(String[] args) {
-        new Metadatos().generarMetadatos(1);
+        new Metadatos().generarMetadatos(3);
     }
 
     /**
@@ -54,7 +55,7 @@ public class Metadatos {
                 System.out.println("Versión del Driver: " + versionDriver);
                 System.out.println("URL               : " + url);
                 System.out.println("Usuario           : " + usuario);
-                System.out.println("=============================================");
+
                 System.out.println();
                 ResultSet resul;
                 ResultSet rsColumnas;
@@ -65,18 +66,35 @@ public class Metadatos {
                     String esquema = resul.getString(2); //columna 2
                     String tabla = resul.getString(3);   //nombre de la tabla
                     String tipo = resul.getString(4);  //tipo
-                    if (nombresTablas.contains(tabla.toLowerCase())) {
+                    if (nombresTablas.contains(tabla.toLowerCase()) && !tabla.equalsIgnoreCase("f")
+                            && !tabla.equalsIgnoreCase("p") && !tabla.equalsIgnoreCase("v")) {
+                        System.out.println("===================================================================");
                         System.out.println(tipo + " - Catalogo: " + catalogo + ", Esquema : " + esquema + ", Tabla : " + tabla);
                         System.out.println();
-
+                        ArrayList<String> clavesPrimarias = new ArrayList<>();
                         try {
                             //Devuelve las PK de cada tabla de la BBDD
                             ResultSet rsp = dbmd.getPrimaryKeys(null, null, tabla);
-                            if (rsp.next())
-                                System.out.println("PK (clave primaria): " + rsp.getObject(4));
+
+                            while (rsp.next())
+                                clavesPrimarias.add(rsp.getObject(4).toString());
+                            //System.out.println("PK (clave primaria): " + rsp.getObject(4));
                             rsp.close();
                         } catch (SQLException ignored) {
                         }
+                        if (clavesPrimarias.size() > 1) {
+                            System.out.print("PKs (claves primarias): ");
+                            boolean primero = true;
+                            for (String clave: clavesPrimarias) {
+                                if (primero) {
+                                    System.out.print(clave);
+                                    primero = false;
+                                } else
+                                    System.out.print(", " + clave);
+                            }
+                            System.out.println();
+                        } else if (clavesPrimarias.size() == 1)
+                            System.out.println("PK (clave primaria): " + clavesPrimarias.get(0));
                         //Mostramos el nombre de las columnas y su tipo de dato
                         rsColumnas = dbmd.getColumns(null, null, tabla, null);
                         System.out.println("Columnas " + tabla);
@@ -84,7 +102,6 @@ public class Metadatos {
                             System.out.format("\t %-17s | %s\n",
                                      rsColumnas.getString(4), rsColumnas.getString("TYPE_NAME"));
                         }
-                        System.out.println("=============================================");
                         rsColumnas.close();
                     }
                 }
